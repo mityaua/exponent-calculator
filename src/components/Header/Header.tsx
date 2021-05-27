d921181b4943c79a75417f5f7c742e37f375e42a
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-05-27 21:44:16 +0300

    Some style fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import s from './Header.module.scss';
 export default function Header() {
   return (
     <header className={s.header}>
-      <h1>Exponent calculator</h1>
+      <h1 className={s.title}>Exponent calculator</h1>
 
       <p>Enter values into two of the input fields to solve for the result.</p>
     </header>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
 
 ReactDOM.render(
